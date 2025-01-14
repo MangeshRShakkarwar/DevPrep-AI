73a47bf669385f3df5c471ba92f91afc40e79d90
MangeshRShakkarwar <mangeshramraoshakkarwar@gmail.com>
2025-01-14 15:01:53 +0530

    made some changes in config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -20,7 +20,7 @@ export default {
   				foreground: 'hsl(var(--popover-foreground))'
   			},
   			primary: {
-  				DEFAULT: '#FFFFFF',
+  				DEFAULT: '#000000',
   				foreground: 'hsl(var(--primary-foreground))'
   			},
   			secondary: {
