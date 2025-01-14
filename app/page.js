c44b1afda881bd541b53e47b214bba9b1f219e81
MangeshRShakkarwar <mangeshramraoshakkarwar@gmail.com>
2025-01-14 15:59:57 +0530

    forms added

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <h1>Mock Interview</h1>
+      <h1>DevPrep AI</h1>
       <p>This is a mock interview platform using AI</p>
       <Button variant="outline" >Get Started</Button>
     </div>
